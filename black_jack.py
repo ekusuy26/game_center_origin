572d60d70088e288e6f90a3437fe2b9e5a7988a4
ekusuy26 <y.yuu6221@gmail.com>
2020-06-13 01:14:04 +0900

    Game rule explanation

---
+++ black_jack.py
@@ -0,0 +1,4 @@
+print ('welcome to BLACK JACK!!')
+print ('this game is a simple card game.')
+print ('Make your hand so that the total number of cards does not exceed [21].')
+print ('The one closer to [21] is the winner!')
