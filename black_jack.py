d617d9e7274a33469c1d538e48ef7a295e094755
ekusuy26 <y.yuu6221@gmail.com>
2020-06-13 22:31:19 +0900

    Create playing cards

--- black_jack.py
+++ black_jack.py
@@ -2,3 +2,12 @@ print ('welcome to BLACK JACK!!')
 print ('this game is a simple card game.')
 print ('Make your hand so that the total number of cards does not exceed [21].')
 print ('The one closer to [21] is the winner!')
+
+marks = ['spade', 'heart', 'diamond', 'club']
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+all = []
+for mark in marks:
+    for num in nums:
+        all.append([mark, num])
+
